53ee0ba70cbc56a16a1a25c822ea05f8d79e83f1
Fernando Carpani <carpani@fing.edu.uy>
2026-04-06 18:35:47 -0300

    Add back class

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,8 +1,80 @@
 import { browser,type Browser} from 'wxt/browser';
 
-var tabId:number;
-var auxB=browser;
+class backPage {
+  id:string="";
+  trace:any[]=[];
+  ports = new Map<number, Browser.runtime.Port|0>();
+  lastPage:number=-1;
+  constructor(){
+    browser.runtime.onMessage.addListener(this.handlePageUp);
+  };
+
+  onConnect(port:Browser.runtime.Port){
+    let tabId = port.sender?.tab?.id!;
+    this.ports.set(tabId, port);
+    port.onMessage.addListener((msg,port)=>{
+      console.log("rcv from ",tabId);
+      this.trace.push({"rcv":msg});
+    });
+
+    port.onDisconnect.addListener(() => {
+      this.ports.delete(tabId)
+    });
+  }
+  // connect(tabId){
+  //   try {
+  //     this.port=browser.tabs.connect({name:this.id});
+  //   } catch (e) {
+  //       this.port=browser.runtime.connect({name:this.id});
+  //   }
+    
+  //   this.port.onMessage.addListener((msg,lPort:Browser.runtime.Port)=>{
+  //     if(this.port?.name!=lPort.name){
+  //       return;
+  //     }
+  //     this.trace.push({"rcv":msg});
+  //   });
+
+  //   this.port.onDisconnect.addListener((port)=>{
+  //     this.port=browser.runtime.connect({name:this.id});
+  //   });
+
+  //   return this.port;
+  // };
+  
+  handlePageUp(msg:any,sender:any,sendResponse:any){
+    console.log(msg,sender);
+    this.lastPage=sender.tab?.id!;
+    this.ports.set(this.lastPage,0);
+  };
+
+  sendMsg(tabId:number, msg:any,type:string=""){
+    let port=this.ports.get(tabId);
+    if(port!=0){
+      port?.postMessage({"orig":"bkg","type":type,"msg":msg});
+      this.trace.push({"sent":{"type":type,"msg":msg}});
+    } else {
+      this.trace.push({"sent":{"type":"error","msg":"port is 0"}});
+    }
+  }
+
+  async imUp(){
+      // avisa al back que esta vivo.
+    browser.runtime.sendMessage({orig:this.id,type:"Up",msg:""})
+      .then((x)=>{
+        console.log("I'm Up RCV:",JSON.stringify(x));
+      })
+      .catch((x)=>{
+        console.log(x);
+        return x;
+      });
+
+  };
+
+}
+
 export default defineBackground(() => {
+   const back=new backPage();
    console.log('Hello background!', { id: browser.runtime.id });
 
   // async function getTab() {
@@ -12,84 +84,85 @@ export default defineBackground(() => {
   //  return tab;
   // };
 
-function getTab() {
-    let tab:Browser.tabs.Tab;
-  return   browser.tabs.query({ active:true })
-   .then(
-      (tabs)=>{
-        tab=tabs[0];
-        console.log("getTab",tabs[0].id,tab.id);
-        tabId=tab.id||-1;
-        return tab;
-      }
-    )
-    .catch(e=>{
-      console.log("error en gettab",e);
-    })
+// function getTab() {
+//     let tab:Browser.tabs.Tab;
+//   return   browser.tabs.query({ active:true })
+//    .then(
+//       (tabs)=>{
+//         tab=tabs[0];
+//         console.log("getTab",tabs[0].id,tab.id);
+//         tabId=tab.id||-1;
+//         return tab;
+//       }
+//     )
+//     .catch(e=>{
+//       console.log("error en gettab",e);
+//     })
 
-  };
+//   };
 
 
 
-  function sendMessage(msg:any,port:Browser.runtime.Port){
-    // port.onDisconnect.addListener(()=>{
-    //   port=browser.tabs.connect(tabId,port);
-    //   sendMessage(msg,port);
-    // });
-    try  {
-      port.postMessage(msg);
-    }
-    catch(e) {
-      console.log("Post Error:",e);
-      port=connect(tabId,port.name);
-      port.postMessage(msg);
-    }
+//   function sendMessage(msg:any,port:Browser.runtime.Port){
+//     // port.onDisconnect.addListener(()=>{
+//     //   port=browser.tabs.connect(tabId,port);
+//     //   sendMessage(msg,port);
+//     // });
+//     try  {
+//       port.postMessage(msg);
+//     }
+//     catch(e) {
+//       console.log("Post Error:",e);
+//       port=connect(tabId,port.name);
+//       port.postMessage(msg);
+//     }
 
-  }
+//   }
 
-  function connect(tabId:number,portName:string){
-    console.log("connect for tab ",tabId,portName); 
-    let port:Browser.runtime.Port=browser.tabs.connect(tabId,{name:portName}); // conexion al canal
-    port.onDisconnect.addListener(()=>{
-      port=browser.tabs.connect(tabId,{name:portName});
-    });
-    port.postMessage({type:"start"});
-    return port;
-  }
+//   function connect(tabId:number,portName:string){
+//     console.log("connect for tab ",tabId,portName); 
+//     let port:Browser.runtime.Port=browser.tabs.connect(tabId,{name:portName}); // conexion al canal
+//     port.onDisconnect.addListener(()=>{
+//       port=browser.tabs.connect(tabId,{name:portName});
+//     });
+//     port.postMessage({type:"start"});
+//     return port;
+//   }
 
-  browser.runtime.onInstalled.addListener(async () => {
-    const tabs = await getTab();
-    console.log("Todas las tabs:", tabs);
-  });
+//   browser.runtime.onInstalled.addListener(async () => {
+//     const tabs = await getTab();
+//     console.log("Todas las tabs:", tabs);
+//   });
 
-async function main(){
-  getTab()
-    .then(tab=>{
-          console.log("tab",tab,browser);
+// function main(){
+// //async function main(){
+//   // getTab()
+//   //   .then(tab=>{
+//   //         console.log("tab",tab,browser);
   
-          let portBack:Browser.runtime.Port;
+//   //         let portBack:Browser.runtime.Port;
 
-          browser.runtime.onMessage.addListener(
-            (msg)=>{
-              console.log("General Msg Rcv:",msg);
-              if(msg.type=="init"){
-                portBack=connect(tabId,"conn-back");
+//   //         browser.runtime.onMessage.addListener(
+//   //           (msg)=>{
+//   //             console.log("General Msg Rcv:",msg);
+//   //             if(msg.type=="init"){
+//   //               portBack=connect(tabId,"conn-back");
 
-                portBack.onMessage.addListener((msg)=>
-                {
-                  console.log(`RCV From ${portBack.name}:`,msg);
-                  sendMessage("rcv front...",portBack);
-                });
-              }
-            }
-        );
-      }
-    )
-  .catch(e=>console.log(e));
+//   //               portBack.onMessage.addListener((msg)=>
+//   //               {
+//   //                 console.log(`RCV From ${portBack.name}:`,msg);
+//   //                 sendMessage("rcv front...",portBack);
+//   //               });
+//   //             }
+//   //           }
+//   //       );
+//   //     }
+//   //   )
+//   // .catch(e=>console.log(e));
 
-}
+// }
 
-main();
+// main();
 });
 
    
--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -2,7 +2,7 @@
 const encoder=new TextEncoder();
 
 import { browser, type Browser } from 'wxt/browser';
-class extPage {
+class frontPage {
   url:string="";
   site:string="";
   id:string="";
@@ -12,7 +12,9 @@ class extPage {
     this.url=location.href;
     this.site=location.host;
     crypto.subtle.digest("SHA-256",encoder.encode(this.url))
-      .then((x)=>{this.id=(new Uint8Array(x)).toString()})
+      .then((x)=>{this.id=(new Uint8Array(x)).toString();
+      this.imUp();
+      })
       .catch((x)=>{console.log(x);return(x);});
       browser.runtime.onMessage.addListener(this.handleGenMsg);
   };
@@ -43,7 +45,7 @@ class extPage {
   };
 
   sendMsg(msg:any,type:string=""){
-    this.port?.postMessage({"orig":this,"type":type,"msg":msg});
+    this.port?.postMessage({"orig":this.id,"type":type,"msg":msg});
     this.trace.push({"sent":{"type":type,"msg":msg}});
   }
 
@@ -66,112 +68,9 @@ export default defineContentScript({
    matches: ["https://listado.mercadolibre.com.uy/inmuebles/apartamentos/*"],
 
    main(ctx) {
-    const currPage=new extPage();
+    const currPage=new frontPage();
     
-  //    function sendMessage(msg:any,port:Browser.runtime.Port){
-  //   // port.onDisconnect.addListener(()=>{
-  //   //   port=browser.tabs.connect(tabId,port);
-  //   //   sendMessage(msg,port);
-  //   // });
-  //   console.log(msg,"--->>",port.name);
-  //   try  {
-  //     port.postMessage(msg);
-  //   }
-  //   catch(e) {
-  //     console.log("Post Error:",e);
-  //     port=connect(port.name);
-  //     if(port)
-  //     port.postMessage(msg);
-  //   }
-
-  // }
-
-  // function connect(portName:string){
-  //   console.log("connect name",portName);
-  //   let port:Browser.runtime.Port=browser.runtime.connect({name:portName}); // conexion al canal
-  //   return port;
-  // }
-  //   let backPort:Browser.runtime.Port;
   
-  //   let continuar=false;
-  //   browser.runtime.sendMessage({type:"init"});
-  //   let countConnect=0;
-  //   console.log("Ini Connect");
-  //   browser.runtime.onConnect.addListener(    
-  //     (port)=>{
-  //       console.log("connect",++countConnect);
-  //       port.onDisconnect.addListener(()=>{
-  //           port=browser.runtime.connect({name:port.name});
-  //         });
-
-  //       port.onMessage.addListener((msg)=>
-  //       {
-  //         sendMessage({type:"Received"},backPort);
-  //       });
-        
-  //       backPort=port;
-
-     
-       
-
-  //     }
-  //   );
-  
-  //   setTimeout(()=>{
-  //     backPort=connect("conn-back");
-  //     sendMessage({type:"onChannel",message:"first"},backPort);
-  //     backPort.onDisconnect.addListener(()=>{
-  //     backPort=browser.runtime.connect({name:backPort.name});
-  //     sendMessage({type:"start"},backPort);
-  //   });
-  //     continuar=true;
-  //   },
-  //   1000);
-
-  //    setInterval(()=>{
-  //         console.log("Sending Message",backPort);
-  //         sendMessage({type:"onChannel" ,message:"onConnect"},backPort);
-  //       },
-  //       5000);
-  //   //while(!continuar);
-
-  //   let app=document.createElement("ol");
-
-  //   let pos=document.querySelector(".brand-wrapper-showroom-desktop")?.parentElement;
-  //   console.log('MI content script');
-  //   pos?.prepend(app);
-  //     backPort=connect("conn-back");
-  //   //sendMessage("hola back...",backPort);
-  //    backPort.onMessage.addListener((message)=>{
-  //       console.log(message);
-  //     });
-  
-  
-     //   });
- 
-    // const ui = createIntegratedUi(ctx, {
-    //   position: 'inline',
-    //   anchor: pos,
-    //   append: 'first',
-    //   onMount: (container) => {
-    //     // Append children to the container
-    //     app.textContent = 'APP CONTENT...';
-    //     container.append(app);
-    //   },
-    //   });
-   
-    // setTimeout(async ()=>{
-    //  browser.runtime.sendMessage({type:"Hola",message:"Hola Back !!"});
-    //     },1000);  
-    // browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    //   app.innerText=JSON.stringify(message,null,2);
-    //   console.log("Content script received message:", {m:message,s:sender});
-    //   sendResponse("QUE PASA Back???");
-    //   return true;
-    // });
-   // pos?.childNodes[0].remove();
-
-    //ui.mount();
    },
    });
 
